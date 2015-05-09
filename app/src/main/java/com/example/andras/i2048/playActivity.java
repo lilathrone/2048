aa7cb3e9e8fe56cb4375c2a92ef26e738a2d1759
endruew <akendrunai@gmail.com>
2015-05-10 00:16:54 +0200

    buttons, sensor fixes, comments, formating

--- app/src/main/java/com/example/andras/i2048/playActivity.java
+++ app/src/main/java/com/example/andras/i2048/playActivity.java
@@ -21,7 +21,6 @@ import android.widget.Toast;
 
 public class playActivity extends ActionBarActivity implements SensorEventListener {
 
-    int End;
     DrawView DW;
     Button UP, DOWN, RIGHT, LEFT, ONOFF;
     boolean on = true;
@@ -31,6 +30,7 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        //jatekmod valaszto dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Select a game mode")
                 .setItems(R.array.mode_array, new DialogInterface.OnClickListener() {
@@ -38,25 +38,23 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
                         Context context = getApplicationContext();
                         String text = "";
 
+                        //valasztas alapjan scorehatar beallitasa + ki toast-ba a vlasztas
                         switch (which) {
                             case 0:
                                 text = "1024";
-                                End = 1024;
                                 DW.gamemode = 1024;
                                 break;
                             case 1:
                                 text = "2048";
-                                End = 2048;
                                 DW.gamemode = 2048;
                                 break;
                             case 2:
                                 text = "Endless";
-                                End = -1;
                                 DW.gamemode = -1;
                                 break;
                         }
 
-                        Toast toast = Toast.makeText(context, DW.gamemode + " mode", Toast.LENGTH_SHORT);
+                        Toast toast = Toast.makeText(context, text + " mode", Toast.LENGTH_SHORT);
                         toast.show();
                     }
                 });
@@ -65,6 +63,7 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
 
         DW = (DrawView) findViewById(R.id.GameView);
 
+        //szenzoros iranyitas ki-be kapcsolasa - default bekapcsolva
         ONOFF = (Button) findViewById(R.id.OnOff);
         ONOFF.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,14 +83,18 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
             }
         });
 
+        //gombokkal valo iranyitas: adott iranyba leptet -> volt lepes? ha igen spawn es ujrarajzolas
+        //ha win/gameover nem tudunk lepni
         UP = (Button) findViewById(R.id.fel);
         UP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (DW.DoUp())
-                DW.Spawn();
-                DW.invalidate();
-
+                if (!DW.gameOver && !DW.win)
+                    if(DW.DoUp())
+                    {
+                        DW.Spawn();
+                        DW.invalidate();
+                    }
             }
         });
 
@@ -99,9 +102,12 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
         DOWN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (DW.DoDown())
-                DW.Spawn();
-                DW.invalidate();
+                if (!DW.gameOver && !DW.win)
+                    if(DW.DoDown())
+                    {
+                        DW.Spawn();
+                        DW.invalidate();
+                    }
             }
         });
 
@@ -109,9 +115,12 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
         RIGHT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (DW.DoRight())
-                DW.Spawn();
-                DW.invalidate();
+                if (!DW.gameOver && !DW.win)
+                    if(DW.DoRight())
+                    {
+                        DW.Spawn();
+                        DW.invalidate();
+                    }
             }
         });
 
@@ -119,29 +128,26 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
         LEFT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (DW.DoLeft())
-                DW.Spawn();
-                DW.invalidate();
+                if (!DW.gameOver && !DW.win)
+                    if(DW.DoLeft())
+                    {
+                        DW.Spawn();
+                        DW.invalidate();
+                    }
             }
         });
 
+        //szenzor
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        //Gyro = (Sensor)sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        //gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        //sensorManager.registerListener(this, gyro, sensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        //sensorManager.registerListener(this, Gyro, SensorManager.SENSOR_DELAY_NORMAL );
-        //sensorManager.registerListener(this, gyro, sensorManager.SENSOR_DELAY_NORMAL);
-           }
+    }
 
     @Override
     protected void onPause() {
@@ -159,76 +165,73 @@ public class playActivity extends ActionBarActivity implements SensorEventListen
     boolean Yback = false;
 
     int TimeToWait = 100;
+
+    //szenzorertek valtozas eseten
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
-
-        /*
-        if (mySensor.getType() == Sensor.TYPE_GYROSCOPE)
-        {
-            float x = event.values[0];
-            float y = event.values[1];
-
-            if (x != last_x)
-            {
-                DW.DoLeft();
-                DW.Spawn();
-                DW.invalidate();
-            }
-
-            last_x = x;
-        }
-        */
-
 
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER && on) {
             float x = event.values[0];
             float y = event.values[1];
             float z = event.values[2];
-            long curTime = System.currentTimeMillis();
+            long curTime = System.currentTimeMillis(); //aktualis idopillanat
 
-
-            //100msec elteltevel nezzuk csak ujra, allando ellenorzes helyett
+            //100msec elteltevel nezzuk csak ujra, allando ellenorzes helyett (aktualis - elozo)
+            //ha volt osszevonas 1,5 mp-kent nezzek ujra
             if ((curTime - lastUpdate) > TimeToWait) {
                 TimeToWait = 100;
                 long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
                 if (Math.abs(x) > 3) {
                     if ( x > -3 && !Xback) {
-                        if (DW.DoLeft())
-                        DW.Spawn();
-                        DW.invalidate();
+                        if (!DW.gameOver && !DW.win)
+                            if(DW.DoLeft())
+                            {
+                                DW.Spawn();
+                                DW.invalidate();
+                            }
                         Xback = true;
                         TimeToWait = 1500;
-                    } else if ( x < 3 && !Xback) {
-                        if (DW.DoRight())
-                        DW.Spawn();
-                        DW.invalidate();
+                    }
+                    else if ( x < 3 && !Xback) {
+                        if (!DW.gameOver && !DW.win)
+                            if(DW.DoRight())
+                            {
+                                DW.Spawn();
+                                DW.invalidate();
+                            }
                         Xback = true;
                         TimeToWait = 1500;
-                    } else if (Xback)
+                    }
+                    else if (Xback)
                         Xback = false;
-                } else if (Math.abs(y) > 3) {
+                }
+                else if (Math.abs(y) > 3) {
                     if (y > -3) {
-                        if (DW.DoDown())
-                        DW.Spawn();
-                        DW.invalidate();
+                        if (!DW.gameOver && !DW.win)
+                            if(DW.DoDown())
+                            {
+                                DW.Spawn();
+                                DW.invalidate();
+                            }
                         Yback = true;
                         TimeToWait = 1500;
-                    } else if (y < 3) {
-                        if (DW.DoUp())
-                        DW.Spawn();
-                        DW.invalidate();
+                    }
+                    else if (y < 3) {
+                        if (!DW.gameOver && !DW.win)
+                            if(DW.DoUp())
+                            {
+                                DW.Spawn();
+                                DW.invalidate();
+                            }
                         Yback = true;
                         TimeToWait = 1500;
-                    } else if (Yback)
+                    }
+                    else if (Yback)
                         Yback = false;
                 }
-
-                last_x = x;
-                last_y = y;
-                last_z = z;
-                lastUpdate = System.currentTimeMillis();
+                lastUpdate = System.currentTimeMillis(); //elozo = aktualis
             }
         }
     }
